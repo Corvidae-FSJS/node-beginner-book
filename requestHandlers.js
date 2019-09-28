b1b7ae61bc58fda6539ae4a0e8b6f5a4898f6c78
Dylan Corvidae <dylancorvidae@gmail.com>
2019-09-28 15:17:22 -0700

    finished, shows png on http://localhost:8888/show

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,8 @@
-var querystring = require("querystring");
+var querystring = require('querystring'),
+    fs = require('fs');
+    formidable = require('formidable')
 
-function start(response, postData) {
+function start(response) {
   console.log(`Request handler 'start' was called.`);
 
   const body = '<html>' +
@@ -22,13 +24,33 @@ function start(response, postData) {
   response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
   console.log("Request handler 'upload' was called"); 
-  response.writeHead(200, {"Content-Type": "test/plain"});
-  response.write("You've sent: " + 
-  querystring.parse(postData).text);
-  response.end();
+
+  var form = new formidable.IncomingForm();
+  console.log("about to parse");
+  form.parse(request, function(error, fields, files) {
+    console.log("parsing done");
+
+    fs.rename(files.upload.path, '/tmp/test.png', function(error) {
+      if (error) {
+        fs.unlink('/tmp/test.png');
+        fs.rename(files.upload.path, '/tmp/test.png');
+      }
+    });
+    response.writeHead(200, {"Content-Type": "test/plain"});
+    response.write("You've sent: " + 
+    querystring.parse(postData).text);
+    response.end();
+  });
+}
+
+function show(response) {
+  console.log("Request handler 'show' was called.");
+  response.writeHead(200, {"Content-Type": "image/png"});
+  fs.createReadStream('/tmp/test.png').pipe(response);
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show; 
